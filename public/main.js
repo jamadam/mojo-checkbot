39021566369c7f75e04cde7167165d5ce659dfcb
sugama <sugama@sv02.cutout.biz>
2011-10-18 11:10:12 +0900

    status code statistics shown in browsser

--- public/main.js
+++ public/main.js
@@ -1,9 +1,10 @@
 $(function() {
     
+    var status_statistics = {};
+    
     function fetch() {
         var offset = $('#result tbody tr').length;
         $.get('/echo?offset='+offset, function(msg){
-            $("#jobs").html(msg.remain);
             $.each(msg.result, function(idx, data){
                 var status = data['res'];
                 var newTr = $('<tr></tr>').addClass('res'+status);
@@ -11,13 +12,21 @@ $(function() {
                     newTr.append('<td>'+col+'</td>');
                 });
                 $("#result tbody").prepend(newTr);
+                status_statistics[status] = (status_statistics[status] || 0) + 1;
+                $("#summary").empty();
+                $("#summary").append(new_stat('Jobs', msg.remain));
+                $("#summary").append(new_stat('Fixed', $('#result tbody tr').length));
+                for (var key in status_statistics) {
+                    $("#summary").append(new_stat(key, status_statistics[key]));
+                }
             });
-            var fixed = $('#result tbody tr').length;
-            $('#fixed').html(fixed);
             setTimeout(fetch, 2000);
         });
     };
     
+    function new_stat(label, data) {
+        return $('<p><span class="label">'+ label+ '</span>: <span class="data">' + data + '</span></p>');
+    }
     fetch();
     
     $("tbody tr").live('click', function() {
