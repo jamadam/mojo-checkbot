c479b23c0d3195ddf2addf484e7387d1978a7d31
sugama <sugama@sv02.cutout.biz>
2011-10-21 11:17:45 +0900

    behaves better on illigal rel path

--- lib/MojoCheckbot.pm
+++ lib/MojoCheckbot.pm
@@ -175,7 +175,7 @@ our $VERSION = '0.01';
         $new->path->canonicalize;
         $new->query($temp->query);
         $new->fragment($temp->fragment); # delete?
-        if ($new->path->parts->[0] && $new->path->parts->[0] =~ /^\./) {
+        while ($new->path->parts->[0] && $new->path->parts->[0] =~ /^\./) {
             shift @{$new->path->parts};
         }
         return $new;
