af1f423fbbdd97fbaa83df9fb50168763f056b19
jamadam <sugama@jamadam.com>
2013-11-12 01:09:05 +0900

    timeout is 0 for now

--- lib/MojoCheckbot.pm
+++ lib/MojoCheckbot.pm
@@ -57,7 +57,7 @@ my %options = (
     sleep               => 1,
     ua                  =>
         "mojo-checkbot/$VERSION (+https://github.com/jamadam/mojo-checkbot)",
-    timeout             => 15,
+    timeout             => 0,
     evacuate            => 30,
     'validator-nu-url'  => 'http://html5.validator.nu/',
 );
